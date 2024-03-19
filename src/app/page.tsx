2b8414672c44557376cac130b807f6b7810d0845
vanderleipinto <vandecopinto@gmail.com>
2024-03-19 07:56:51 -0300

    feat: Renderização Condicional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
 import { Card } from "@/components/Card";
-import { Circle } from "@/components/Circle";
-import { Geo, } from "@/components/Geo";
-import { Person } from "@/components/Person";
 
 
 function Page(){
   return(
     <div>
-      <h1>Olá mundo</h1>
-      
-      <Card>
-      <>
-          <h1 className="text-3xl font-bold italic"> Tudo que vier aqui será children lá no componente.</h1>
-          <p className="text-right text-sm"> "Author descohecido"</p>
-          <p> Essa children por estar envolvida em blanckets (fragment) será considerada um jsx, o que exige um type 
-            (children: ReactNode;)
-            diferente lá no componente. </p>
-            <Circle/>
-      </>
-      </Card>
-    </div>
+      <h1>Olá mundo</h1>      
+      <Card phrase="Alguma frase" author="Coach de milhões"/> 
+      <Card phrase="Alguma frase" />  
+    </div> 
   )
 }
 
