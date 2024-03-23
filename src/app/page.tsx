49e3f14aa62952d41e30b05c1d9c46e51e6842b5
vanderleipinto <vandecopinto@gmail.com>
2024-03-23 11:51:26 -0300

    feat: Rating emoji hard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 import { Rate } from '@/components/Rate';
 import { peopleList } from '@/data/peopleList'
 
-function Page(){
-  
-  const fullTime = new Intl.DateTimeFormat('pt-BR', {
-    timeStyle: 'short',
-    hour12: false
-  }).format(); 
-
-  const hour  = new Date().getHours();  
+function Page(){ 
   
   return(
     <div className='w-screen h-screen flex flex-col justify-center items-center text-white
     bg-gradient-to-r from-gray-900 to-gray-800'>
       <div className='text-9xl'>
-        <Rate rate={3.6}/>
-      </div>
-      
-      </div>
+        <Rate rate={6}/> 
+      </div>      
+    </div>
   )
 }
-export default Page;  
+export default Page;
