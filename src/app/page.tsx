9957844960039d00cb1041595bcf911941a72c27
vanderleipinto <vandecopinto@gmail.com>
2024-03-27 21:06:11 -0300

    feate: Trabalhando com States

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,20 @@
 "use client";
 
 import { CustomButtom } from "@/components/CustomButtom";
-import { FormEvent } from "react";
+import { useState } from "react";
 
 export default function Page() {
 
-  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // Previne o comportamento padrão do form (que é fazer um get para enviar o formulário)
-    alert("opa, mandando");
-  };
+  const [count, setCount] = useState(0);  
+  
+  const handeClickButton = () => {
+    setCount(count +1);
+  }
 
   return (
     <div className="w-screen h-screen  bg-slate-700 flex flex-col justify-center items-center">
-      
-      <h1 className="text-5xl mb-3 bg-blue-300">Form de login</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input className="p-3 m-3 text-black" type="text" name="" id="" />
-        <input type="submit" value="enviar" />
-      </form>
-      <form onSubmit={(e)  => {e.preventDefault}}>
-      </form>
-    </div>
-
-    
+      <p>{count}</p>
+      <button onClick={handeClickButton} className="bg-blue-500">+1</button>
+    </div>    
   );
 }
