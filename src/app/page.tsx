8dcd715f104f4959445852e6a1c5afa5662b76c6
vanderleipinto <vandecopinto@gmail.com>
2024-03-27 21:24:58 -0300

    feat: Usando states para auxiliar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { CustomButtom } from "@/components/CustomButtom";
 import { useState } from "react";
 
 export default function Page() {
 
   const [count, setCount] = useState(0);  
+  const [show, setShow] = useState(false)
+
   
   const handeClickButton = () => {
-    setCount(count +1);
+     setShow(!show)
   }
 
   return (
     <div className="w-screen h-screen  bg-slate-700 flex flex-col justify-center items-center">
       <p>{count}</p>
-      <button onClick={handeClickButton} className="bg-blue-500">+1</button>
+      <button onClick={handeClickButton} className="bg-blue-500">{show ? 'Ocultar' : 'Mostrar'}Área secreta</button>
+      {show &&
+      <div className="w p-3 bg-blue-300 rounded-md mt-3">Área secreta</div>
+      }
     </div>    
   );
 }
